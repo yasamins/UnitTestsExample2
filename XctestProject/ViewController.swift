ad3ca7ee275ed0e793d12f61e82f8a1b7fd9d267
yasamins <yasamin.salami@yahoo.com>
2017-02-19 00:20:27 +0200

    Initial Commit

---
+++ XctestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XctestProject
+//
+//  Created by yasamin  on 19.2.2017.
+//  Copyright © 2017 yasamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
